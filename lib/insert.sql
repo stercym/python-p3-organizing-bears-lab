0cff9e31746e7692585d6c6137392890070128c1
Stercy Mutuku <mutukustercy93@gmail.com>
2025-08-27 14:51:01 +0300

    Performs CRUD operations on the databas using SQL

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 20, "M", "chocolate", "sanguine", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 25, "M", "brown", "melancholic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 24, "F", "fair", "choleric", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 29, "M", "dark", "phlegmatic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 19, "F", "light", "choleric", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 24, "F", "fair", "choleric", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 29, "F", "dark", "phlegmatic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, NULL, NULL, NULL, NULL, 1);
+
+
